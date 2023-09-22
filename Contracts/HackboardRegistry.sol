41a354057ff15e01ca5e7345bd530681cd45c42a
Darius Tanz <scratchticketseb@gmail.com>
2023-09-22 20:03:42 +0000

    Sep 22, 2023, 4:03 PM

--- Contracts/HackboardRegistry.sol
+++ Contracts/HackboardRegistry.sol
@@ -3,9 +3,7 @@ pragma solidity 0.8.19;
 contract HackBoardRegistry{
     address HackBoardAdmin;
     address Handler;
-    Hyperlane Mailbox = Hyperlane(0x35231d4c2D8B8ADcB5617A638A0c4548684c7C70);
-    Hyperlane GasProcessorContract = Hyperlane(0x56f52c0A1ddcD557285f7CBc782D3d83096CE1Cc);
-    uint256[] SupportedChains;
+
     string[] AllTeams;
 
     constructor(){
@@ -23,7 +21,7 @@ contract HackBoardRegistry{
     }
 
     function OnboardNewTeam(string TeamName, address[] CurrentMembers) public {
-        
+
     }
 
     function handle(uint32 _origin, bytes32 _sender, bytes calldata _body) external {
@@ -31,11 +29,4 @@ contract HackBoardRegistry{
         HackBoardTeam memory ArrivingInfo = abi.decode(_body, (HackBoardTeam));
 
     }
-}
-
-interface Hyperlane {
-    function dispatch(uint32 _destinationDomain, bytes32 _recipientAddress, bytes calldata _messageBody ) external returns (bytes32);
-    function process(bytes calldata _metadata, bytes calldata _message) external;
-    function payForGas(bytes32 _messageId, uint32 _destinationDomain, uint256 _gasAmount, address _refundAddress) external payable;
-    function quoteGasPayment(uint32 _destinationDomain, uint256 _gasAmount) external view returns (uint256);
 }
