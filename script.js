54d2cb6a8df69134c334d51f7d54997b6470886f
Darius Tanz <scratchticketseb@gmail.com>
2023-09-22 21:31:50 +0000

    Sep 22, 2023, 5:31 PM

--- script.js
+++ script.js
@@ -2,10 +2,41 @@ let accounts;
 let provider;
 let signer;
 
+async function signIn() {
+    try {
+        // Check if MetaMask is installed
+        if (typeof window.ethereum !== 'undefined' || (typeof window.web3 !== 'undefined')) {
+            // Ethereum user detected. You can now use the provider.
+            provider = new ethers.BrowserProvider(window.ethereum);
 
+            // Request account access
+            await window.ethereum.enable();
 
+            // Getting accounts
+            accounts = await provider.listAccounts();
+            if (accounts.length == 0) {
+                console.log('No account found! Make sure the Ethereum client is configured correctly.');
+                return null;
+            } else {
+                console.log(`Account: ${accounts[0]}`);
+            }
 
+            signer = await provider.getSigner();
 
+            await getAddress()
+            await getEthBalance()
+            await initToken()
+            await getBalance()
+
+        } else {
+            console.log('Please install MetaMask!');
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
 
 
 
