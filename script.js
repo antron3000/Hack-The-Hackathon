83c2c0a3c4c2a30988534a85ac0506e498b54200
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 00:44:55 +0000

    Sep 22, 2023, 8:44 PM

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.getElementById('metamaskButton').addEventListener('click', async () => 
             console.log("a")
 
             document.getElementById('metamaskButton').innerText = "Connected";
-            document.getElementById('temprow')
+            document.getElementById('temprow').style.display = "none";
 
         } catch (error) {
             console.error("User denied account access or an error occurred.");
