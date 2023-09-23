5f4562fba044446d26952f1a4b646db96d073ba8
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 00:22:03 +0000

    Sep 22, 2023, 8:22 PM

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-let accounts;
+let ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"uint256","name":"TeamID","type":"uint256"},{"internalType":"address[]","name":"NewMembers","type":"address[]"}],"name":"AddTeamMember","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"AllTeams","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"AllUsers","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"GetAllTeams","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"TeamID","type":"uint256"}],"name":"GetTeamInfo","outputs":[{"components":[{"internalType":"address","name":"Admin","type":"address"},{"internalType":"string","name":"TeamName","type":"string"},{"internalType":"string","name":"ShortDescription","type":"string"},{"internalType":"string","name":"Discord","type":"string"},{"internalType":"string","name":"MainBountyTarget","type":"string"},{"internalType":"address[]","name":"TeamMembers","type":"address[]"},{"internalType":"bool","name":"InterestedInPredictionMarket","type":"bool"}],"internalType":"struct HackBoardRegistry.HackBoardTeam","name":"","type":"tuple"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_User","type":"address"}],"name":"GetUserInfo","outputs":[{"components":[{"internalType":"bool","name":"HasTeam","type":"bool"},{"internalType":"uint256","name":"TeamID","type":"uint256"}],"internalType":"struct HackBoardRegistry.User","name":"","type":"tuple"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"HackBoardAdmin","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"TeamName","type":"string"},{"internalType":"string","name":"ShortDescription","type":"string"},{"internalType":"string","name":"Discord","type":"string"},{"internalType":"string","name":"MainBountyTarget","type":"string"},{"internalType":"address[]","name":"CurrentMembers","type":"address[]"},{"internalType":"bool","name":"InterestedInPredictionMarket","type":"bool"}],"name":"OnboardNewTeam","outputs":[{"internalType":"uint256","name":"TeamCode","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"TeamIncrement","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"Teams","outputs":[{"internalType":"address","name":"Admin","type":"address"},{"internalType":"string","name":"TeamName","type":"string"},{"internalType":"string","name":"ShortDescription","type":"string"},{"internalType":"string","name":"Discord","type":"string"},{"internalType":"string","name":"MainBountyTarget","type":"string"},{"internalType":"bool","name":"InterestedInPredictionMarket","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"Users","outputs":[{"internalType":"bool","name":"HasTeam","type":"bool"},{"internalType":"uint256","name":"TeamID","type":"uint256"}],"stateMutability":"view","type":"function"}]
+
+let contractAddress = "0xD87dF59Bf476e9700f36F00c198166bC901a0e17"
 let provider;
-let signer;
 
-async function signIn() {
-    try {
-        // Check if MetaMask is installed
-        if (typeof window.ethereum !== 'undefined' || (typeof window.web3 !== 'undefined')) {
-            // Ethereum user detected. You can now use the provider.
-            provider = new ethers.BrowserProvider(window.ethereum);
-
+document.getElementById('metamaskButton').addEventListener('click', async () => {
+    console.log("button clicked")
+    if (typeof window.ethereum !== 'undefined') {
+        console.log("defined")
+        try {
             // Request account access
-            await window.ethereum.enable();
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
 
-            // Getting accounts
-            accounts = await provider.listAccounts();
-            if (accounts.length == 0) {
-                console.log('No account found! Make sure the Ethereum client is configured correctly.');
-                return null;
-            } else {
-                console.log(`Account: ${accounts[0]}`);
-            }
+            provider = new ethers.BrowserProvider(window.ethereum);
+            console.log("a")
+            const signer = await provider.getSigner();
+            console.log("a")
 
-            signer = await provider.getSigner();
+            const userAddress = await signer.getAddress();
+            console.log("a")
 
+            console.log(userAddress)
+            document.getElementById('addressDisplay').innerText = `Address: ${userAddress}`;
 
-        } else {
-            console.log('Please install MetaMask!');
-            return null;
+            // Fetch balance
+            console.log(userAddress)
+            const balanceWei = await provider.getBalance(userAddress);
+            console.log(balanceWei)
+            const balanceEth = ethers.formatEther(balanceWei);
+            console.log(balanceEth)
+            document.getElementById('balanceDisplay').innerText = `Balance: ${balanceEth} ETH`;
+
+        } catch (error) {
+            console.error("User denied account access or an error occurred.");
         }
-    } catch (error) {
-        console.error(error);
-        return null;
+    } else {
+        console.error("MetaMask is not installed");
     }
-}
-
-
-
+});
 
 document.addEventListener("DOMContentLoaded", function() {
     // Define the start time and duration (in this case, a 48-hour hackathon)
