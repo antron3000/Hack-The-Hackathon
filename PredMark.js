e43cdcfdcea63135cac35c9c0244363bd8fdb9d9
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 19:36:25 +0000

    Sep 23, 2023, 3:36 PM

--- PredMark.js
+++ PredMark.js
@@ -106,7 +106,8 @@ function insertDataIntoTable(dataone, datatwo, TeamID) {
     const teamName = dataone[1];
     const LongInterest = datatwo[0] / 1000000000000000000;
     const ShortInterest = datatwo[1] / 1000000000000000000;
-    const UserFadeDposit = PredictionsContract.UserForDeposits
+    const UserFadeDposit = PredictionsContract.UserForDeposits("0xc932b3a342658A2d3dF79E4661f29DfF6D7e93Ce", 0)
+
 
     const tbody = document.getElementById('Registry');
 
