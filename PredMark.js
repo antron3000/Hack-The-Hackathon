5734c72b450702c5ee4e467bafa6cca0a3f33baf
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 19:22:28 +0000

    Sep 23, 2023, 3:22 PM

--- PredMark.js
+++ PredMark.js
@@ -104,7 +104,7 @@ function insertDataIntoTable(dataone, datatwo) {
     // Add console logs to debug the dataone
 
     const teamName = dataone[1];
-    
+    const 
 
     console.log(teamName, description, contactLink, interestInPredictionMarket, mainSponsorPrizeTarget);
 
