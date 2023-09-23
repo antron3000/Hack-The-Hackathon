c3db39645d2c4347ccba8288ce11d4e4dde47aba
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 02:19:11 +0000

    Sep 22, 2023, 10:19 PM

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ async function GetAllTeamInfoToConsole(){
     let TeamInfos;
     let teamIDs = await GetAllTeams();
     for (let i = 0; i < teamIDs.length; i++) {
-        console.log(await GetTeamInfo(teamIDs[i]));
+        Team
     }
     console.log(teamIDs)
     return(teamIDs)
