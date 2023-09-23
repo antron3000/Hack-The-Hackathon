f61e3f9a4f057036430f69a5369582a623f4f822
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 12:55:44 +0000

    Sep 23, 2023, 8:55 AM

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ async function GetTeamInfo(teamID) {
 
 async function GetUserInfo(){
     let User = await contract.GetUserInfo(await signer.getAddress());
-    return JSON.parse(JSON.stringify(User))
+    return JSON.parse(JSON.stringify(User { value: bigIntValue.toString() }))
 }
 
 async function GetAllTeamInfoToConsole(){
