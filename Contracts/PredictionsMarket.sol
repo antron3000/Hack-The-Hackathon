cb54da7a00556013c86490d41685c03ce124ee01
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 16:24:24 +0000

    Sep 23, 2023, 12:24 PM

--- Contracts/PredictionsMarket.sol
+++ Contracts/PredictionsMarket.sol
@@ -81,7 +81,8 @@ contract HackBoardPredictionMarket{
         for(uint256 i = 0; i < ParticipatingTeams.length; i++){
             if(!TeamPredictionsInfo[ParticipatingTeams[i]].ForSuccess){
                 TeamPredictionsInfo[ParticipatingTeams[i]].AgainstSuccessful = true;
-                //Calculate the winner payout rate, which is the total available prize pool devided by the total amount of ether deposited into the fadeprizepool for this team
+                //Calculate the winner payout rate, which is the total fade available prize pool devided by the total amount of ether deposited into the fadeprizepool for this team
+                TeamPredictionsInfo[ParticipatingTeams[i]].WinnerPayoutRate = TotalFadeAvailablePrizePool / TeamPredictionsInfo[ParticipatingTeams[i]].TotalFadePredictionDeposits;
             }
         }
     }
