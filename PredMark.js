2dcc4e357367ed073447a92d250f0e968ba6fab9
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 19:24:43 +0000

    Sep 23, 2023, 3:24 PM

--- PredMark.js
+++ PredMark.js
@@ -114,7 +114,7 @@ function insertDataIntoTable(dataone, datatwo) {
     const tr = document.createElement('tbody');
     tr.innerHTML = `<tr>
         <td>${teamName}</td>
-        <td>Long interest: ${description}</td>
+        <td>Long interest: ${LongInterest}</td>
         <td>${contactLink}</a></td>
         <td>${interestInPredictionMarket}</td>
         </tr>
