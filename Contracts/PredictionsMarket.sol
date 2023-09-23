b1afe158c288493a8967cee9358c52e4dce202b8
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 15:47:17 +0000

    Sep 23, 2023, 11:47 AM

--- Contracts/PredictionsMarket.sol
+++ Contracts/PredictionsMarket.sol
@@ -2,14 +2,82 @@
 
 pragma solidity ^0.8.0;
 
-import "./HackBoardRegistry.sol";
-
 contract HackBoardPredictionMarket{
     HackBoardRegistry public HackBoardRegistryContract;
     uint256[] public ParticipatingTeams;
 
     constructor(){
-        //Check 
+        //check hackboard
     }
 }
 
+contract HackBoardRegistry{
+    address public HackBoardAdmin;
+    uint256[] public AllTeams;
+    address[] public AllUsers;
+    uint256 public TeamIncrement;
+
+    constructor(){
+        HackBoardAdmin = msg.sender;
+    }
+
+    mapping(address => User) public Users;
+    mapping(uint256 => HackBoardTeam) public Teams;
+
+    struct User{
+        bool HasTeam;
+        uint256 TeamID;
+    }
+    
+    struct HackBoardTeam{
+        address Admin;
+        string TeamName;
+        string ShortDescription;
+        string Discord;
+        string MainBountyTarget;
+        address[] TeamMembers;
+        bool InterestedInPredictionMarket;
+    }
+
+    function OnboardNewTeam(string memory TeamName, string memory ShortDescription, string memory Discord, string memory MainBountyTarget, address[] memory CurrentMembers, bool InterestedInPredictionMarket) public returns(uint256 TeamCode) {
+        uint256 TeamID = TeamIncrement;
+        TeamIncrement++;
+
+        Users[msg.sender].HasTeam = true;
+        Users[msg.sender].TeamID = TeamID;
+
+        Teams[TeamID] = HackBoardTeam(msg.sender, TeamName, ShortDescription, Discord, MainBountyTarget, CurrentMembers, InterestedInPredictionMarket);
+
+        for(uint256 i = 0; i < CurrentMembers.length; i++){
+            Users[CurrentMembers[i]].HasTeam = true;
+            Users[CurrentMembers[i]].TeamID = TeamID;
+        }
+        
+        AllUsers.push(msg.sender);
+        AllTeams.push(TeamID);
+        return TeamID;
+    }
+
+    function AddTeamMember(uint256 TeamID, address[] memory NewMembers) public {
+        require(Teams[TeamID].Admin == msg.sender);
+
+        for(uint256 i = 0; i < NewMembers.length; i++){
+            Users[NewMembers[i]].HasTeam = true;
+            Users[NewMembers[i]].TeamID = TeamID;
+            Teams[TeamID].TeamMembers.push(NewMembers[i]);
+        }
+    }
+
+    function GetTeamInfo(uint256 TeamID) public view returns(HackBoardTeam memory){
+        return Teams[TeamID];
+    }
+
+    function GetUserInfo(address _User) public view returns(User memory){
+        return(Users[_User]);
+    }
+
+    function GetAllTeams() public view returns(uint256[] memory){
+        return AllTeams;
+    }
+
+}
