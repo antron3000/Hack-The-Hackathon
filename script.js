f83edbd6d138724fd03f34e757941b53576877f2
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 02:46:45 +0000

    Sep 22, 2023, 10:46 PM

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function insertDataIntoTable(data) {
     tr.innerHTML = `<tr>
         <td>${teamName}</td>
         <td>${description}</td>
-        <td><a style="text-decoration: none; " href="${contactLink}">Discord</a></td>
+        <td><a style="text-decoration: none; color: orange;" href="${contactLink}">Discord</a></td>
         <td>${interestInPredictionMarket}</td>
         <td>${mainSponsorPrizeTarget}</td>
         </tr>
