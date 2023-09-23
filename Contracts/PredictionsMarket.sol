9d9c839dafe9ae886e3485387abc0b9eb3995d43
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 17:40:35 +0000

    Sep 23, 2023, 1:40 PM

--- Contracts/PredictionsMarket.sol
+++ Contracts/PredictionsMarket.sol
@@ -105,7 +105,7 @@ contract HackBoardPredictionMarket{
     //View Functions
 
     function TeamSuccessfulCheck(uint256 TeamID) public view returns(bool ForSuccess, bool AgainstSuccess){
-        return(TeamPredictionsInfo[TeamID].For);
+        return(TeamPredictionsInfo[TeamID].ForSuccess, TeamPredictionsInfo[TeamID].AgainstSuccess);
     }
 }
 
