0ee6f264f890d1e3bf9e5f924a704c1008523f05
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 18:58:46 +0000

    Sep 23, 2023, 2:58 PM

--- PredMark.js
+++ PredMark.js
@@ -76,7 +76,7 @@ async function SetUserTeamName(){
 
 async function GetTeamPredictionsInfo(teamID){
     let TeamInfo = await PredictionsContract.GetTeamInfo(teamID);
-    TeamInfo = [JSON.parse(JSON.stringify(TeamInfo))];
+    //TeamInfo = [JSON.parse(JSON.stringify(TeamInfo))];
     return TeamInfo
 }
 
