6fab36303603d350f4aa070c14620a731bd4d97e
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 16:07:29 +0000

    Sep 23, 2023, 12:07 PM

--- Contracts/PredictionsMarket.sol
+++ Contracts/PredictionsMarket.sol
@@ -26,7 +26,7 @@ contract HackBoardPredictionMarket{
                 ParticipatingTeams.push(AllTeams[i]);
                 TeamParticipating[AllTeams[i]] = true;
                 //for each participating team, create two new erc20 tokens, one for the team, one for fading the team
-                ERC20 ForToken = new PredictionsDerivativeToken()
+                PredictionsDerivativeToken ForToken = new PredictionsDerivativeToken()
 
             }
         }
