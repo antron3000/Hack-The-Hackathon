502856e93f13380720ed824e317c7150a7a6c288
Darius Tanz <scratchticketseb@gmail.com>
2023-09-23 19:50:10 +0000

    Sep 23, 2023, 3:50 PM

--- PredMark.js
+++ PredMark.js
@@ -125,7 +125,9 @@ async function insertDataIntoTable(dataone, datatwo, TeamID) {
     tbody.appendChild(tr);
 }
 
-async function IncreaseInterest()
+async function IncreaseInterest(LongShort, TeamID){
+    
+}
 
 async function addGnosisChainToMetaMask() {
     const chainId = 100;
