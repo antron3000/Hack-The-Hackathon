1730e44540074e0ce3ef66352d949f2c2a63eb32
Darius Tanz <scratchticketseb@gmail.com>
2023-10-19 22:38:16 +0000

    Oct 19, 2023, 6:38 PM

--- Contracts/Hackboard.sol
+++ Contracts/Hackboard.sol
@@ -27,6 +27,7 @@ contract HackBoardRegistry{
         string Discord;
         string MainBountyTarget;
         address[] TeamMembers;
+        address
     }
 
     function OnboardNewTeam(string memory TeamName, string memory ShortDescription, string memory Discord, string memory MainBountyTarget, address[] memory CurrentMembers, bool InterestedInPredictionMarket) public {
