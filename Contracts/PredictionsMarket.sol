5f9480dd73c79652f4fe51f4f6e455240dc0d62d
Darius Tanz <scratchticketseb@gmail.com>
2023-10-19 22:07:00 +0000

    Oct 19, 2023, 6:07 PM

--- Contracts/PredictionsMarket.sol
+++ Contracts/PredictionsMarket.sol
@@ -111,9 +111,8 @@ contract HackBoardPredictionMarket{
 
         for(uint256 i = 0; i < SuccessfulTeamsWithDeposits.length; i++){
             TeamPredictionsInfo[SuccessfulTeams[i]].ForSuccess = true;
-            if(TeamPredictionsInfo[SuccessfulTeams[i]].TotalForPredictionsDeposits > 0){
+            
                 TeamPredictionsInfo[SuccessfulTeams[i]].WinnerPayoutRate = (TotalForAvailablePrizePool * 1000) / TeamPredictionsInfo[SuccessfulTeams[i]].TotalForPredictionsDeposits;
-            }
         }
 
         //------------------------------------------------
