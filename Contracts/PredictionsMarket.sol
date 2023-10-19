76dfc88fcd572e48ba7db1fedc77d6a6b766e4ba
Darius Tanz <scratchticketseb@gmail.com>
2023-10-19 21:58:59 +0000

    Oct 19, 2023, 5:58 PM

--- Contracts/PredictionsMarket.sol
+++ Contracts/PredictionsMarket.sol
@@ -96,7 +96,7 @@ contract HackBoardPredictionMarket{
         require(SuccessfulTeams.length == 10);
         CloseMarkets();
 
-        //check all successful teams if they have any deposits, if not, remove them from the tot
+        //check all successful teams if they have any deposits, if not, remove them from the total wi
 
         uint256 TotalForAvailablePrizePool = TotalForPrizePool / WinningSlots;
 
