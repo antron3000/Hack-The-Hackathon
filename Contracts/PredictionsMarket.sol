67b1a5a30cc117a685604ab58cc609c1e8311dad
Darius Tanz <scratchticketseb@gmail.com>
2023-10-19 21:40:54 +0000

    Oct 19, 2023, 5:40 PM

--- Contracts/PredictionsMarket.sol
+++ Contracts/PredictionsMarket.sol
@@ -9,6 +9,7 @@ contract HackBoardPredictionMarket{
     uint256 public WinningSlots;
     uint256 public TotalForPrizePool;
     uint256 public TotalFadePrizePool;
+    uint256 public 
     bool public MarketsOpen;
 
     mapping(uint256 => bool) public TeamParticipating;
