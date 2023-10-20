468d58f829068262c2f7d9143b2d1648fb8893aa
Darius Tanz <scratchticketseb@gmail.com>
2023-10-20 19:24:12 +0000

    Oct 20, 2023, 3:24 PM

--- Contracts/PredictionsMarket.sol
+++ Contracts/PredictionsMarket.sol
@@ -38,7 +38,7 @@ contract HackBoardPredictionMarket{
     function DepositToTeam(uint256 TeamID, bool ForAgainst, uint256 Amount) public {
         require(MarketsOpen);
         require(HackBoardToken.allowance(msg.sender, address(this)) >= Amount, "Insufficient allowance");
-        require(Amount > 0);
+        require(Amount > 1 ether, "Minimum ");
         require(TeamParticipating[TeamID]);
 
         if(ForAgainst){
