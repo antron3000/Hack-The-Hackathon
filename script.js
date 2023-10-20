f66fc85cfa9f617ad1e1a1b6ebffbc6132de322d
Darius Tanz <scratchticketseb@gmail.com>
2023-10-20 20:55:52 +0000

    Oct 20, 2023, 4:55 PM

--- script.js
+++ script.js
@@ -149,3 +149,30 @@ async function populateTableWithTeamInfo() {
         
     
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Define the start time and duration (in this case, a 48-hour hackathon)
+    const startTime = new Date('2023-10-22T21:00:00');  // Adjust the date and time as needed
+    const durationInMilliseconds = 32400 * 100;
+    const endTime = new Date(startTime.getTime() + durationInMilliseconds);
+
+    function updateCountdown() {
+        const now = new Date();
+        let timeDifference = endTime - now;
+
+        // Calculate hours, minutes, and seconds
+        const hours = Math.floor(timeDifference / (1000 * 60 * 60));
+        timeDifference -= hours * (1000 * 60 * 60);
+
+        const mins = Math.floor(timeDifference / (1000 * 60));
+        timeDifference -= mins * (1000 * 60);
+
+        const secs = Math.floor(timeDifference / 1000);
+
+        // Update the countdown timer display
+        document.getElementById('timer').textContent = `${hours}:${mins < 10 ? '0' : ''}${mins}:${secs < 10 ? '0' : ''}${secs}`;
+    }
+
+    // Update the countdown every second
+    setInterval(updateCountdown, 1000);
+});
